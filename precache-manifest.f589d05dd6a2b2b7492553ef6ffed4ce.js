206973ed92c1ef9922179000c02365f04ea16ac0
Jhorman967 <jhorman967@gmail.com>
2022-01-18 01:55:45 -0500

    Updates

---
+++ precache-manifest.f589d05dd6a2b2b7492553ef6ffed4ce.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e775bab3c4a5cd6656fc771b3d46dc46",
+    "url": "/FloppyCS.github.io/index.html"
+  },
+  {
+    "revision": "c7685e64075a1769fad4",
+    "url": "/FloppyCS.github.io/static/css/main.be1bfac2.chunk.css"
+  },
+  {
+    "revision": "eedf27a3308f1f55b68c",
+    "url": "/FloppyCS.github.io/static/js/2.b9959374.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/FloppyCS.github.io/static/js/2.b9959374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7685e64075a1769fad4",
+    "url": "/FloppyCS.github.io/static/js/main.d0bc6756.chunk.js"
+  },
+  {
+    "revision": "8ea8ce1dc6771ab3921f",
+    "url": "/FloppyCS.github.io/static/js/runtime-main.580fc678.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/FloppyCS.github.io/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "9a5af346e91878b75a2017a5e1f85e0f",
+    "url": "/FloppyCS.github.io/static/media/features-split-image-01.9a5af346.png"
+  },
+  {
+    "revision": "bed789a3d8abba51ec7801a9d1d893fa",
+    "url": "/FloppyCS.github.io/static/media/features-split-image-02.bed789a3.png"
+  },
+  {
+    "revision": "471b5bdbf4b09921677ed13c491c4569",
+    "url": "/FloppyCS.github.io/static/media/features-split-image-03.471b5bdb.png"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/FloppyCS.github.io/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/FloppyCS.github.io/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "5bb0eeb3478bde38051b12a829e29600",
+    "url": "/FloppyCS.github.io/static/media/logo.5bb0eeb3.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/FloppyCS.github.io/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
